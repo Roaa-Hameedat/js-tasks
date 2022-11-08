386caa0d38a4425c35b014aaa739901469c88dbd
Roaa-Hameedat <roaaomar9898@gmail.com>
2022-11-08 08:02:35 +0200

    7-11

--- 7-11/task3.js
+++ 7-11/task3.js
@@ -1,19 +1,15 @@
-
-
-
-function shorterInArray (arraynames){
-    let  x=arraynames[0].length;
-    let y =arraynames[0];
-for(i=0; i<arraynames.length; i++){
-   if(x>arraynames[i+1].length){
-    x=arraynames[i+1].length;
-    y=arraynames[i+1];
-   }
-}
+function shorterInArray (arrayname){
+    let  x = arrayname[0].length;
+    let y = arrayname[0];
+for(i=0; i<arrayname.length; i++)
+{
+   if( x > arrayname[i].length){
+    x=arrayname[i].length;
+    y=arrayname[i];
+   } }
 return y;
 }
 
 let arrayname=["roaa","rahmah","wesam","yazeed","aya"];
-var c =shorterInArray(arrayname);
 
-document.write(c);
+ document.write(shorterInArray(arrayname));
